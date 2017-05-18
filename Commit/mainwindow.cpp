de3cc8a60275562a44533caaee5aa11eeaf4e334
Siniewicz <tadzik333@interia.eu>
2017-05-18 23:01:38 +0200

    Create mainwindow.cpp

---
+++ Commit/mainwindow.cpp
@@ -0,0 +1,48 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->plot->AddGraph();
+    ui->plot->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->plot->graph(0)->setLineStyle(QCPGraph::lsNone);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::addPoint(double x, double y)
+{
+    qv_x.append(x);
+    qv_y.append(y);
+}
+
+void MainWindow::ClearData()
+{
+    qv_x.clear();
+    qv_y.clear();
+}
+
+void MainWindow::plot()
+{
+    ui->plot->graph(0)->setData(qv_x, qv_y);
+    ui->plot->replot();
+    ui->plot->update();
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    addPoint(ui->bx_x->value(), ui->bx_y->value());
+    plot();
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    clearData();
+    plot();
+}
