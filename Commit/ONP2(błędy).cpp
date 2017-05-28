b466a7fe1a5ace2d717f85da1603d88b58246017
sylwow <Sylwo103@interia.pl>
2017-05-28 11:37:28 +0200

    Add files via upload

--- Commit/ONP2(błędy).cpp
+++ Commit/ONP2(błędy).cpp
@@ -119,3 +119,102 @@ bool PobierzSymbolONP(float* liczba, AnsiString tab, int* okz, AnsiString* str)
   *str = "";
   if (*okz <= tab.Length())
   {
+znak = tab[*okz];
+  }
+  else
+  {
+    znak = '\0';
+  }
+  if (znak == ',') {return (false);};
+  while (znak == '0' || znak == '1' || znak == '2' || znak == '3' || znak == '4' || znak == '5' || znak == '6' || znak == '7' || znak == '8' || znak == '9' || znak == ',')
+  {
+    if (znak == ',')
+    {
+      liczbaprzecinkow++;
+    };
+    if (liczbaprzecinkow == 2)
+    {
+      return (false);
+    };
+    zmp2 = false;
+    string = string + znak;
+    *okz = *okz + 1;
+    if (*okz <= tab.Length()) {znak = tab[*okz];};
+    if (*okz == tab.Length() && *okz > 1 && (znak == '0' || znak == '1' || znak == '2' || znak == '3' || znak == '4' || znak == '5' || znak == '6' || znak == '7' || znak == '8' || znak == '9'))
+    {
+      if ((tab[tab.Length() - 1] >= '0' && tab[tab.Length() - 1] <= '9') || tab[tab.Length() - 1] == ',')
+      {
+        string = string + znak;
+        *okz = *okz + 1;
+      }
+    };
+    if (*okz >= tab.Length()) {break;};
+    if (zmp == true) {zmp = false;};
+  };
+  if (zmp == true && zmp2 == true)
+  {
+    if (znak == 'x')
+    {
+      *str = znak;
+      *okz = *okz + 1;
+      return (true);
+    };
+//----------------tu nastepuje zmiana--------------------
+    if (znak == '+' || znak == '-' || znak == '*' || znak == '/' || znak == '(' || znak == ')')
+    {
+      if (znak == '+' || znak == '-')
+      {
+        bool poczatek = false; //poczatek znaku minus;
+        if (*okz == 1) {poczatek = true;};
+        if (*okz != 1)
+        {
+          if (tab[*okz - 1] == '(')
+          {
+            poczatek = true;
+          };
+        };
+        int liczbaminusow = 0;
+        if (znak == '-') {liczbaminusow++;};
+        *okz = *okz + 1;
+        if ((tab[*okz] >= '0' && tab[*okz] <= '9') || (tab[*okz] == 'x') || (tab[*okz] == '(') || (tab[*okz] == ')') || (tab[*okz] == '*') || (tab[*okz] == '/'))
+        {
+          if (*str == '+')
+          {
+            *str = '+';
+            return (true);
+          }
+          else
+          {
+            if (poczatek)
+            {
+              *str = 'N';
+              return (true);
+            };
+            *str = znak;
+            return (true);
+          }
+        };
+        while (tab[*okz] == '+' || tab[*okz] == '-')
+        {
+          if (tab[*okz] == '-')
+          {
+            liczbaminusow++;
+          };
+          *okz = *okz + 1;
+        };
+        if (Parzysta(liczbaminusow))
+        {
+          if (poczatek)
+          {
+            *str = '+';
+            return (true);
+          };
+          *str = '+';
+        }
+        else
+        {
+          if (poczatek)
+          {
+            *str = 'N';
+            return (true);
+          };
