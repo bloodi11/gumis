9511870b1ada499eaa8205648006b8cbb0140a73
sylwow <Sylwo103@interia.pl>
2017-05-28 11:36:01 +0200

    Add files via upload

---
+++ Commit/ONP2(błędy).cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+void KonwerterONP(AnsiString tab, int* okz, AnsiString* str)
+{
+  TStos* wierzcholek;
+  Stos stos(wierzcholek);
+  AnsiString s;
+  float* liczba;
+  float zmp;
+  liczba = &zmp;
+  char zn; char* st;
+  TDane element;
+
+    do
+    {
+      PobierzSymbolONP(liczba, tab, okz, str);
+      if ((*str >= '0' && *str <= "9999999999") || *str == 'x')
+      {
+        s = s + *str + ' ';
+      }
+      else
+      {
+        zn = *str->c_str();
+        if (zn == ')')
+        {
